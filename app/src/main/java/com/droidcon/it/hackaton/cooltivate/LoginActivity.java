8fd946f8d6681d0598d6085653317eac50b4093d
Luciano Quercia <luciano.quercia@gmail.com>
2015-04-11 19:57:35 +0200

    Sistemate le 2 Activity

--- app/src/main/java/com/droidcon/it/hackaton/cooltivate/LoginActivity.java
+++ app/src/main/java/com/droidcon/it/hackaton/cooltivate/LoginActivity.java
@@ -1,16 +1,11 @@
 package com.droidcon.it.hackaton.cooltivate;
 
 import android.app.Activity;
-import android.graphics.Color;
-import android.view.View;
-import android.webkit.WebView;
-import android.widget.TextView;
+import android.content.Intent;
 import android.widget.Toast;
 
 import org.androidannotations.annotations.AfterInject;
-import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
-import org.androidannotations.annotations.ViewById;
 
 import io.relayr.RelayrSdk;
 import io.relayr.model.User;
@@ -19,32 +14,11 @@ import rx.Observer;
 import rx.android.schedulers.AndroidSchedulers;
 
 @EActivity(R.layout.activity_main)
-public class MainActivity extends Activity {
+public class LoginActivity extends Activity {
 
-    @ViewById
-    WebView webView;
-    @ViewById
-    TextView temperature;
-    @ViewById
-    TextView humidity;
-    @ViewById
-    TextView lux;
-
-    @AfterViews
-    public void loadWebView() {
-        temperature.setText("+25° C");
-        humidity.setText("70%");
-        lux.setText("100 nit");
-
-        webView.loadUrl("http://192.168.43.1:8080/browserfs.html");
-        webView.setInitialScale(100);
-        webView.setBackgroundColor(Color.BLACK);
-        webView.setOverScrollMode(View.OVER_SCROLL_NEVER);
-        webView.setVerticalScrollbarOverlay(false);
-    }
 
     @AfterInject
-     void onCreate() {
+    void onCreate() {
         RelayrSdkInitializer.initSdk(this);
         if (!RelayrSdk.isUserLoggedIn()) {
             logIn();
@@ -57,18 +31,20 @@ public class MainActivity extends Activity {
                 .subscribe(new Observer<User>() {
                     @Override
                     public void onCompleted() {
+                        Intent intent = new Intent(LoginActivity.this, CameraActivity.class);
+                        startActivity(intent);
                     }
 
                     @Override
                     public void onError(Throwable e) {
-                        Toast.makeText(MainActivity.this,
+                        Toast.makeText(LoginActivity.this,
                                 R.string.unsuccessfully_logged_in, Toast.LENGTH_SHORT).show();
                     }
 
 
                     @Override
                     public void onNext(User user) {
-                        Toast.makeText(MainActivity.this,
+                        Toast.makeText(LoginActivity.this,
                                 R.string.unsuccessfully_logged_in, Toast.LENGTH_SHORT).show();
                     }
                 });
